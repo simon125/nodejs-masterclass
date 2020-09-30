176906fa593b9345c3d88cd415c6bab1d08830d7
Szymon Oleszek <s.oleszek@polsource.com>
2020-09-30 17:07:00 +0200

    2/05 http & the node http module

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end();
+});
+
+const PORT = 5002;
+
+server.listen(PORT, () => console.log("Server running on PORT: 5000"));
