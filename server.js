d9b9e811b6660e5b74a5495d0169c61db02ff012
Szymon <szymqwerty@gmail.com>
2020-10-04 22:02:19 +0200

    start exploring express - setup of masterclass project

--- server.js
+++ server.js
@@ -1,42 +1,14 @@
-const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-  { id: 1, text: 'todo 1' },
-  { id: 2, text: 'todo 2' },
-  { id: 3, text: 'todo 3' },
-];
+dotenv.config({ path: './config/config.env' });
 
-const server = http.createServer((req, res) => {
-  // 1 way to set headers
-  //   res.statusCode = 200;
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.setHeader('X-Powerd-By', 'Node.js');
+const app = express();
 
-  // 2 way to set headers
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js',
-  });
+const PORT = process.env.PORT || 5000;
 
-  //   console.log(req.headers.authorization);
-  let body = [];
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      console.log(body);
-      const data = Buffer.concat(body).toString();
-      console.log(data);
-    });
-
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todos,
-    })
-  );
-});
-
-const PORT = 5000;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
