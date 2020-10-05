68620741b11c3f5dd7057ae4d14b365ef18238b1
Szymon <szymqwerty@gmail.com>
2020-10-05 22:10:16 +0200

    setup and connect mongoDB
    - added mongoose and colors to dependencies
    - connected mongoDB

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const app = express();
 
@@ -14,8 +18,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => {
+    return process.exit(1);
+  });
+});
